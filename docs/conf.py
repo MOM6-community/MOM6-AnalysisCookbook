3ed330b37d72a4145a36f4c2c3b70401401dd59e
Raphael Dussin <raphael.dussin@gmail.com>
2020-11-23 11:06:58 -0500

    make content wider

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,7 @@ nbsphinx_execute = "never"
 # a list of builtin themes.
 #
 html_theme = 'default'
+html_style = '_static/theme.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
