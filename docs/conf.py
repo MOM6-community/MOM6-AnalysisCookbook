25c15bc6a82699ad09a1bb340e1877f9141beb22
raphael dussin <raphael.dussin@gmail.com>
2023-11-08 09:43:24 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,7 @@ nbsphinx_execute = "never"
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
