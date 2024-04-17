9facce516ee9bc954df353a193021ec5691c64c1
raphael dussin <raphael.dussin@gmail.com>
2024-04-17 10:37:49 -0400

    update paths,... (#28)

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 setuptools.setup(
     name="MOM6-AnalysisCookbook",
     version="0.0.1",
-    author="Raphael Dussin",
-    author_email="raphael.dussin@gmail.com",
+    author="Raphael Dussin,...",
+    author_email="",
     description=("A package to do MOM6 data analysis"),
     license="GPLv3",
     keywords="",
-    url="https://github.com/raphaeldussin/MOM6-AnalysisCookbook",
+    url="https://github.com/MOM6-community/MOM6-AnalysisCookbook",
     packages=['MOM6_AnalysisCookbook']
 )
